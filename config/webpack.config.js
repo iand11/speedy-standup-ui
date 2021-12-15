0b1e0a964ce1a785940f9091f1a0ef7bd3b7805b
Ian Driscoll <ian.driscoll.o@gmail.com>
2021-12-14 23:28:51 -0700

    Update webpack.config to update envs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -584,7 +584,7 @@ module.exports = function (webpackEnv) {
             : undefined
         )
       ),
-      new Dotenv(),
+      new Dotenv({ systemvars: true }),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358

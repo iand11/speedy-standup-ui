5f525b7e9309a9009896d80a65af264ec29ce440
Ian Driscoll <ian.driscoll.o@gmail.com>
2021-12-14 23:34:09 -0700

    Hand write env in prod config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -584,7 +584,11 @@ module.exports = function (webpackEnv) {
             : undefined
         )
       ),
-      new Dotenv({ systemvars: true }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'BASE_URL': JSON.stringify(process.env.BASE_URL),
+        }
+      }),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358
